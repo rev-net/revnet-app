05539ad3d6d2146a6693eeaa4fbf27382ab85883
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-12-31 06:35:16 +1000

    static?

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
