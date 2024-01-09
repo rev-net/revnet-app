875d1e5efec4becb5c1741db937c855efa0aeccc
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2024-01-09 21:25:18 +1000

    feat: support optimism sepolia

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -2,8 +2,20 @@ import { ConnectKitButton } from "@/components/ConnectKitButton";
 import Link from "next/link";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { sepolia, useNetwork } from "wagmi";
+import { optimismSepolia } from "viem/chains";
+
+const NAMES: {
+  [chainId: number]: string;
+} = {
+  [sepolia.id]: "Sepolia",
+  [optimismSepolia.id]: "OP Sepolia",
+};
 
 export function Nav() {
+  const { chain } = useNetwork();
+  const chainName = chain ? NAMES[chain.id] : undefined;
+
   return (
     <nav className="text-zinc-50 bg-zinc-900">
       <div className="flex justify-between items-center container py-3">
@@ -16,7 +28,7 @@ export function Nav() {
               alt="Revnet logo"
             />
           </Link>
-          <Badge variant="success">Sepolia</Badge>
+          {chainName ? <Badge variant="success">{chainName}</Badge> : null}
         </div>
 
         <ConnectKitButton />
