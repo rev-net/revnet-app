8ad62cb6f5fb299f4a203c36e8d52330e69b6a99
aeolian <94939382+aeolianeth@users.noreply.github.com>
2024-06-28 06:56:45 +1000

    Base

--- src/lib/wagmiConfig.ts
+++ src/lib/wagmiConfig.ts
@@ -20,7 +20,7 @@ export const wagmiConfig = createConfig(
         `https://optimism-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
       ),
       [baseSepolia.id]: http(
-        `https://base-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
+        `https://api.developer.coinbase.com/rpc/v1/base-sepolia/${process.env.NEXT_PUBLIC_BASE_ID}`
       ),
       [arbitrumSepolia.id]: http(
         `https://arbitrum-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
