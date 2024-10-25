cd76d1ff65de12f6ed6845949d1737b6d63c0363
mejango <mejango@protonmail.com>
2024-10-25 12:01:23 -0300

    cash out tooltip spacing

--- src/app/[chain]/[id]/components/NetworkDetailsTable.tsx
+++ src/app/[chain]/[id]/components/NetworkDetailsTable.tsx
@@ -112,43 +112,49 @@ export function NetworkDetailsTable() {
       <SectionTooltip name="Rules" info="">
       <div className="max-w-md space-y-4 p-2">
         <h3 className="font-medium text-black-500">Stages</h3>
-        <p className="text-sm text-black-300">
+        <p className="text-sm text-black-400">
           This revnet’s rules change automatically in sequential stages. It's stages are permanent once the revnet is deployed.
         </p>
 
         <div className="space-y-2">
         <div className="space-y-1">
           <h3 className="font-medium text-black-500">Timing</h3>
-          <p className="text-sm text-black-300">Determines when a stage becomes active, and how long it lasts before the next stage takes effect.</p>
+          <p className="text-sm text-black-400">Determines when a stage becomes active, and how long it lasts before the next stage takes effect.</p>
         </div>
 
         <div className="space-y-1">
           <h3 className="font-medium text-black-500">Issuance</h3>
-          <p className="text-sm text-black-300">Determines how many {formatTokenSymbol(token)} are created when this revnet receives funds during a stage.</p>
-          <p className="text-sm text-black-400 italic">
+          <p className="text-sm text-black-400">Determines how many {formatTokenSymbol(token)} are created when this revnet receives funds during a stage.</p>
+          <p className="text-sm text-black-300 italic">
             Note: If an AMM exists that is offering a better rate for $BAN than this stage's issuance, inbound payments will be used to buyback from this market instead of being absorbed by the revnet. Splits will still apply to bought back {formatTokenSymbol(token)}.
           </p>
         </div>
 
         <div className="space-y-1">
           <h3 className="font-medium text-black-500">Split</h3>
-          <p className="text-sm text-black-300">Determines how much of {formatTokenSymbol(token)} issuance is set aside to be split among recipients defined by the split operator during a stage.</p>
-          <p className="text-sm text-black-300">The operator is theaccount that can change the split recipients, within the permanent split amount of a stage.</p>
-          <p className="text-sm text-black-400 italic">
+          <p className="text-sm text-black-400">Determines how much of {formatTokenSymbol(token)} issuance is set aside to be split among recipients defined by the split operator during a stage.</p>
+          <p className="text-sm text-black-400">The operator is the account that can change the split recipients, within the permanent split amount of a stage.</p>
+          <p className="text-sm text-black-300 italic">
             Note:  The operator is not bound by stages. The operator can hand off this responsibility to another address at any time, or relinquish it altogether.
           </p>
         </div>
 
         <div className="space-y-1">
           <h3 className="font-medium text-black-500">Automint</h3>
-          <p className="text-sm text-black-300">Determines the amount that can be minted on demand to chosen recipients once the stage starts.</p>
+          <p className="text-sm text-black-400">Determines the amount that can be minted on demand to chosen recipients once the stage starts.</p>
         </div>
 
         <div className="space-y-1">
           <h3 className="font-medium text-black-500">Cash out tax</h3>
-          <p className="text-sm text-black-300">Determines how much of this revnet’s funds can be withdrawn by burning {formatTokenSymbol(token)} during a stage.</p>
-          <p className="text-sm text-black-400 italic">
-            Note: The higher the tax, the less that can be accessed by cashing out at any given time, and the more that is left to share between remaining {formatTokenSymbol(token)} holders who cash out later. Given 100 {nativeTokenSymbol} in the revnet, 100 total supply of {formatTokenSymbol(token)}, and 10 {formatTokenSymbol(token)} being cashed out, a tax rate of 0 would yield a cash out value of 10 {nativeTokenSymbol}, 0.2 would yield 8.2 {nativeTokenSymbol}, 0.5 would yield 5.5 {nativeTokenSymbol}, and 0.8 would yield 2.8 {nativeTokenSymbol}.  This works on a bonding curve with the formula `y = (ax/s) * ((1-r) + xr/s)` where: `r` is the cash out tax rate (from 0 to 1), `a` is the total amount in the revnet being accessed, `s` is the current token supply of {formatTokenSymbol(token)}, `x` is the amount of {formatTokenSymbol(token)} being cashed out.
+          <p className="text-sm text-black-400">Determines how much of this revnet’s funds can be withdrawn by burning {formatTokenSymbol(token)} during a stage.</p>
+          <p className="text-sm text-black-300 italic">
+            Note: The higher the tax, the less that can be accessed by cashing out at any given time, and the more that is left to share between remaining {formatTokenSymbol(token)} holders who cash out later. 
+          </p>
+          <p className="text-sm text-black-300 italic">
+            Given 100 {nativeTokenSymbol} in the revnet, 100 total supply of {formatTokenSymbol(token)}, and 10 {formatTokenSymbol(token)} being cashed out, a tax rate of 0 would yield a cash out value of 10 {nativeTokenSymbol}, 0.2 would yield 8.2 {nativeTokenSymbol}, 0.5 would yield 5.5 {nativeTokenSymbol}, and 0.8 would yield 2.8 {nativeTokenSymbol}.  This works on a bonding curve with the formula `y = (ax/s) * ((1-r) + xr/s)` where: `r` is the cash out tax rate (from 0 to 1), `a` is the total amount in the revnet being accessed, `s` is the current token supply of {formatTokenSymbol(token)}, `x` is the amount of {formatTokenSymbol(token)} being cashed out.
+          </p>
+          <p className="text-sm text-black-300 italic">
+            This works on a bonding curve with the formula `y = (ax/s) * ((1-r) + xr/s)` where: `r` is the cash out tax rate (from 0 to 1), `a` is the total amount in the revnet being accessed, `s` is the current token supply of {formatTokenSymbol(token)}, `x` is the amount of {formatTokenSymbol(token)} being cashed out.
           </p>
         </div>
 
