b0f49986dfe017de62d8da1ca0a83f9ad3d51657
mejango <mejango@protonmail.com>
2024-11-16 23:58:28 -0300

    better pay confirm

--- src/components/ui/stat.tsx
+++ src/components/ui/stat.tsx
@@ -4,8 +4,8 @@ export function Stat({
 }: React.PropsWithChildren<{ label: string }>) {
   return (
     <div>
-      <div className="font-bold text-xl">{label}</div>
-      <div className="text-2xl">{children}</div>
+      <div className="font-bold text-xl text-black">{label}</div>
+      <div className="text-2xl text-black">{children}</div>
     </div>
   );
 }
