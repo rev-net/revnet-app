34ab578cf4c42e309b9698eae5095a733261b798
mejango <mejango@protonmail.com>
2024-11-18 02:01:55 -0300

    understated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,15 @@ export default function Page() {
       <div className="flex flex-col items-left justify-left">
         <Image
           src="/assets/img/revnet-logo.svg"
-          width={200}
-          height={200}
+          width={140}
+          height={140}
           alt="Revnet logo"
         />
         <span className="sr-only">Revnet</span>
-        <div className="text-2xl md:text-3xl mt-4 font-medium text-left">
-          Revnets: unstoppable funding machines for growth-oriented projects on the open internet
+        <div className="text-2xl md:text-2xl mt-4 font-medium text-left">
+          Revnets are unstoppable funding machines for growth-oriented projects on the open internet
         </div>
-        <div className="flex gap-4 mt-10">
-          <Link href="/create">
-            <Button className="md:h-20 h-16 text-xl md:text-2xl px-10 flex gap-2 bg-teal-500 hover:bg-teal-600">
-              Design yours
-            </Button>
-          </Link>
-        </div>
-      </div>
-
-      <div className="mt-20 max-w-prose text-l md:text-2xl text-lg text-left">
-        <div className="mt-10 mb-8 text-xl md:text-2xl text-left">
+        <div className="mt-4 mb-6 text-xl md:text-xl text-left">
           <span className="mr-1">Browse:</span> $<Link href="https://revnet.app/sepolia/1" target="_blank"
             rel="noopener norefererr"
             className="underline hover:text-black/70">NANA</Link><span className="text-zinc-300">{" | "}</span> $<Link href="https://revnet.app/sepolia/3" target="_blank"
@@ -38,6 +28,16 @@ export default function Page() {
             rel="noopener norefererr"
             className="underline hover:text-black/70">CPN</Link>
         </div>
+        <div className="flex gap-4">
+          <Link href="/create">
+            <Button className="md:h-12 h-16 text-xl md:text-xl px-4 flex gap-2 bg-teal-500 hover:bg-teal-600">
+              Design yours
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="mt-14 max-w-prose text-l md:text-2xl text-lg text-left">
         {/* <p className="text-xl">
           Made for creators, developers, communities, investors, and customers. 
         </p> */}
