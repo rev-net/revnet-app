45af9bd4810f6dad02d57133b9b9f9924213c451
mejango <mejango@protonmail.com>
2024-11-18 16:12:31 -0300

    unborken link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Page() {
         </p>
         <div className="flex">
             <div className="mt-8 bg-white text-black text-lg">
-          Plan your revnet with the community <Link href="https://discord.gg/8qdtvdep" className="underline hover:text-black/70">on Discord.
+          Plan your revnet with the community <Link href="https://discord.gg/vhVxwh8aD9" className="underline hover:text-black/70">on Discord.
           </Link>
         </div>
             </div>
