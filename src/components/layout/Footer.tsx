26cf3cc3dff8518f458f3e3ed794e1d0aad5d2d4
mejango <mejango@protonmail.com>
2024-11-18 16:02:20 -0300

    global computer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
       <div className="container border-x-zinc-500 py-10">
         <div>
           <div className="text-md text-zinc-700">
-            <p>Revnets are open source, and enforced by the Revnet, Juicebox, and Uniswap protocols on Ethereum global computer.</p>
+            <p>Revnets are open source, and enforced by the Revnet, Juicebox, and Uniswap protocols on the Ethereum global computer.</p>
             <p>Huge gratitude to the ETH, JBX, REV, and UNI communities for crafting the infrastructure and economy to make revnets possible.</p>
           </div>
         </div>
