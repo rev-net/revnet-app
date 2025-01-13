082faf8c86ab9a58ff9190570c56b7e4db32895e
jigglyjams <jigglyjams@protonmail.com>
2025-01-13 10:35:49 -0500

    feat(create): add toast for encoding errors

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -13,11 +13,11 @@ import { DeployRevnetForm } from "./form/DeployRevnetForm";
 import { parseDeployData } from "./helpers/parseDeployData";
 import { pinProjectMetadata } from "./helpers/pinProjectMetaData";
 import { parseSuckerDeployerConfig } from "./helpers/parseSuckerDeployerConfig";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Page() {
   const [isLoadingIpfs, setIsLoadingIpfs] = useState<boolean>(false);
-
-  const chain = useChain();
+  const { toast } = useToast();
   const {
     write,
     response,
@@ -47,16 +47,27 @@ export default function Page() {
         suckerDeployerConfig: suckerDeployerConfig,
       });
 
-      const encodedData = encodeFunctionData({
-        abi: revDeployerAbi, // ABI of the contract
-        functionName: "deployFor",
-        args: deployData,
-      });
+      try {
+        const encodedData = encodeFunctionData({
+          abi: revDeployerAbi, // ABI of the contract
+          functionName: "deployFor",
+          args: deployData,
+        });
 
-      return {
-        data: encodedData,
-        chain: Number(chainId),
-        deployer: revDeployerAddress[chainId],
+        return {
+          data: encodedData,
+          chain: Number(chainId),
+          deployer: revDeployerAddress[chainId],
+        }
+      } catch (e: any) {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description:
+            e.message ||
+            "Error encoding transaction",
+        });
+        throw e;
       }
     });
 
