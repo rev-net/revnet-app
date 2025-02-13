c5910437b7f4ff5c3095f95dc6a3926805c044a0
mejango <mejango@protonmail.com>
2025-02-12 22:24:43 -0300

    even less words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Page() {
         />
         <span className="sr-only">Revnet</span>
         <div className="text-2xl md:text-2xl mt-8 font-medium text-left">
-          Tokenized revenue for projects on the open internet
+          Tokenized revenue for the open internet
         </div>
         <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
           <div className="flex gap-4 mt-8">
