24ba4b5a46f26124b4d5d217fae944f783f964ec
jigglyjams <jigglyjams@protonmail.com>
2025-02-13 12:17:34 -0500

    fix(alchemy): do the alchemy thang

--- src/lib/wagmiConfig.ts
+++ src/lib/wagmiConfig.ts
@@ -5,25 +5,29 @@ import {
   optimismSepolia,
   sepolia,
 } from "viem/chains";
-import { createConfig, http } from "wagmi";
+import { createConfig, http, fallback } from "wagmi";
 
 const defaultConfig = getDefaultConfig({
   chains: [sepolia, optimismSepolia, baseSepolia, arbitrumSepolia],
   appName: "REVNET",
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   transports: {
-    [sepolia.id]: http(
-      `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
-    ),
-    [optimismSepolia.id]: http(
-      `https://optimism-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
-    ),
-    [baseSepolia.id]: http(
-      `https://api.developer.coinbase.com/rpc/v1/base-sepolia/${process.env.NEXT_PUBLIC_BASE_ID}`
-    ),
-    [arbitrumSepolia.id]: http(
-      `https://arbitrum-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`
-    ),
+    [sepolia.id]: fallback([
+      http("https://eth-sepolia.g.alchemy.com/v2/Y7igjs135LhJTJbYavxq9WlhuAZQVn03"),
+      http(`https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
+    [optimismSepolia.id]: fallback([
+      http("https://opt-sepolia.g.alchemy.com/v2/Y7igjs135LhJTJbYavxq9WlhuAZQVn03"),
+      http(`https://optimism-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
+    [baseSepolia.id]: fallback([
+      http(`https://api.developer.coinbase.com/rpc/v1/base-sepolia/${process.env.NEXT_PUBLIC_BASE_ID}`),
+      http("https://base-sepolia.g.alchemy.com/v2/Y7igjs135LhJTJbYavxq9WlhuAZQVn03"),
+    ]),
+    [arbitrumSepolia.id]: fallback([
+      http("https://arb-sepolia.g.alchemy.com/v2/Y7igjs135LhJTJbYavxq9WlhuAZQVn03"),
+      http(`https://arbitrum-sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
   },
 });
 
