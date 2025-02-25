01924e3ca33b14992ae8b44f1b2b0d10c7697139
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-02-26 07:29:29 +1000

    fix: urn

--- src/app/[jbUrn]/page.tsx
+++ src/app/[jbUrn]/page.tsx
@@ -1,11 +1,26 @@
 import { Nav } from "@/components/layout/Nav";
-import { jbUrn } from "juice-sdk-core";
+import { JB_CHAINS, JBChainId, jbUrn } from "juice-sdk-core";
 import { Providers } from "./Providers";
 import { NetworkDashboard } from "./components/NetworkDashboard/NetworkDashboard";
 
 export default function Page({ params }: { params: { jbUrn: string } }) {
-  const { projectId, chainId } = jbUrn(decodeURIComponent(params.jbUrn)) ?? {};
-  if (!projectId || !chainId) return null;
+  let projectId: bigint | undefined;
+  let chainId: JBChainId | undefined;
+  try {
+    const urn = jbUrn(decodeURIComponent(params.jbUrn));
+    projectId = urn?.projectId;
+    chainId = urn?.chainId;
+
+    if (!projectId || !chainId || !JB_CHAINS[chainId]) {
+      throw new Error("Invalid URN");
+    }
+  } catch (error) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center">
+        Not found
+      </div>
+    );
+  }
 
   return (
     <Providers chainId={chainId} projectId={projectId}>
