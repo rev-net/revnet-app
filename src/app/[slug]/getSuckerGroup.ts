e1b60210a501580ac0f403d3392a506d70a337db
Riderway <riderway@runbox.com>
2025-09-08 20:53:03 +0200

    Add getSuckerGroup function and integrate into YouPage
    - Introduced `getSuckerGroup` function to fetch sucker group data with caching.
    - Updated `YouPage` to utilize `getSuckerGroup` for retrieving projects.
    - Modified `YouSection` to handle loading states and display project data correctly.

---
+++ src/app/[slug]/getSuckerGroup.ts
@@ -0,0 +1,27 @@
+import {
+  SuckerGroupDocument,
+  SuckerGroupQuery,
+  SuckerGroupQueryVariables,
+} from "@/generated/graphql";
+import { getBendystrawClient } from "@/graphql/bendystrawClient";
+import { unstable_cache } from "next/cache";
+
+export const getSuckerGroup = unstable_cache(
+  async (suckerGroupId: string, chainId: number) => {
+    try {
+      const client = getBendystrawClient(chainId);
+      const result = await client.request<SuckerGroupQuery, SuckerGroupQueryVariables>(
+        SuckerGroupDocument,
+        { id: suckerGroupId },
+      );
+      return result.suckerGroup;
+    } catch (err) {
+      console.error((err as any).message);
+      return null;
+    }
+  },
+  ["getSuckerGroup"],
+  {
+    revalidate: 300, // 5 minutes in seconds
+  },
+);
