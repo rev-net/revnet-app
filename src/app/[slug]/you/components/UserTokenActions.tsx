e90da36980d5c7cc9c27b13573a6c3caa097e83b
Riderway <riderway@runbox.com>
2025-10-30 20:14:57 +0100

    Hide bridging for projects 1-4

--- src/app/[slug]/you/components/UserTokenActions.tsx
+++ src/app/[slug]/you/components/UserTokenActions.tsx
@@ -73,7 +73,7 @@ export function UserTokenActions(props: Props) {
           </BorrowDialog>
         ) : null}
 
-        {projects.length > 1 && (
+        {projects.length > 1 && projectId > 4 && (
           <BridgeDialog projects={projects}>
             <Button variant="outline" disabled={creditBalance.value === 0n}>
               Move between chains
